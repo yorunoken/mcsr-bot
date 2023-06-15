a6c54b92b58f076bedbaeb81a6f9b256ba073a3e
Yoru <69751661+YoruNoKen@users.noreply.github.com>
2023-06-15 15:52:52 +0300

    reworked rank system

--- src/utilities/findRank.js
+++ src/utilities/findRank.js
@@ -1,52 +1,46 @@
-const coal = "<:coal:1092904874791931965>";
-const iron = "<:iron:1092904882584965230>";
-const gold = "<:gold:1092904879749611678>";
-const diamond = "<:diamond:1092904877744734288>";
-
+const { ranks } = require("./ranks.js");
 const tierRanges = {
-  "Coal 1": [0, 199],
-  "Coal 2": [200, 399],
-  "Coal 3": [400, 599],
-  "Iron 1": [600, 799],
-  "Iron 2": [800, 999],
-  "Iron 3": [1000, 1199],
-  "Gold 1": [1200, 1399],
-  "Gold 2": [1400, 1599],
-  "Gold 3": [1600, 1799],
-  "Diamond 1": [1800, 1999],
-  "Diamond 2": [2000, 2199],
-  "Diamond 3": [2200, 2399],
+  "Coal 1": [0, 399],
+  "Coal 2": [400, 499],
+  "Coal 3": [500, 599],
+  "Iron 1": [600, 699],
+  "Iron 2": [700, 799],
+  "Iron 3": [800, 899],
+  "Gold 1": [900, 999],
+  "Gold 2": [1000, 1099],
+  "Gold 3": [1100, 1199],
+  "Emerald 1": [1200, 1299],
+  "Emerald 2": [1300, 1399],
+  "Emerald 3": [1400, 1499],
+  "Diamond 1": [1500, 1649],
+  "Diamond 2": [1650, 1799],
+  "Diamond 3": [1800, 1999],
+  Netherite: [2000, Infinity],
 };
 
 function findTier(elo) {
-  let _elo = elo;
-  let tier = "Unknown";
-  let nextTier = "Unknown";
+  let currElo = elo;
+  let tier = "Unrated";
+  let nextTier = "Coal 1";
   let eloNeeded = 0;
 
   for (const [key, value] of Object.entries(tierRanges)) {
-    if (_elo >= value[0] && _elo <= value[1]) {
+    if (elo === 0) break;
+    if (currElo >= value[0] && currElo <= value[1]) {
       tier = key;
       const nextTierIndex = Object.keys(tierRanges).indexOf(key) + 1;
 
       if (nextTierIndex < Object.keys(tierRanges).length) {
         nextTier = Object.keys(tierRanges)[nextTierIndex];
-        eloNeeded = tierRanges[nextTier][0] - _elo;
+        eloNeeded = tierRanges[nextTier][0] - currElo;
       }
       break;
     }
   }
 
-  const emotes = {
-    coal: coal,
-    iron: iron,
-    gold: gold,
-    diamond: diamond,
-  };
-
   function tierToEmote(tier) {
     let _tier = tier.toLowerCase();
-    for (const [key, value] of Object.entries(emotes)) {
+    for (const [key, value] of Object.entries(ranks)) {
       if (_tier.includes(key)) {
         return value;
       }
@@ -54,9 +48,9 @@ function findTier(elo) {
     return null;
   }
 
-  const emote = tierToEmote(tier);
+  const emote = tierToEmote(tier) ?? ranks.unknown;
   const nextEmote = tierToEmote(nextTier);
-  return { currElo: _elo, tier: tier, nextTier: nextTier, eloNeeded: eloNeeded, emote: emote, nextEmote: nextEmote };
+  return { currElo, tier, nextTier, eloNeeded, emote, nextEmote };
 }
 
 module.exports = { findTier };
---
+++ src/utilities/ranks.js
@@ -0,0 +1,10 @@
+const ranks = {
+  unknown: "<:unknown:1118881573660536933>",
+  coal: "<:coal:1118881587497541713>",
+  iron: "<:iron:1118881578685309008>",
+  gold: "<:gold:1118881581147369493>",
+  diamond: "<:diamond:1118881584771256471>",
+  emerald: "<:emerald:1118881582950916117>",
+  netherite: "<:netherite:1118881577049538650>",
+};
+module.exports = { ranks };
